f3b92dbbed7ea9fbe0e6d76210d4854d50cff431
Titus von der Malsburg <malsburg@gmail.com>
2014-02-03 11:55:02 +0000

    Added roxygen2 documentation for detect.fixations.

--- saccades/R/saccade_recognition.R
+++ saccades/R/saccade_recognition.R
@@ -1,7 +1,51 @@
 
-# This function takes a data frame containing raw eyetracking samples
-# and returns a data frame containing fixations.
-detect.fixations <- function(samples, vel.threshold=10, smooth=T) {
+#' Takes a data frame containing raw eyetracking samples and returns a
+#' data frame containing fixations.
+#'
+#' @title Detect fixations in a stream of raw eyetracking samples
+#' @param samples a data frame containing the raw samples as recorded
+#' by the eyetracker.  This data frame has four columns:
+#' \describe{
+#'  \item{time:}{the time at which the sample was recorded}
+#'  \item{trial:}{the trial to which the sample belongs}
+#'  \item{x:}{the x-coordinate of the sample}
+#'  \item{y:}{the y-coordinate of the sample}
+#' }
+#' Samples have to be listed in chronological order.  The velocity
+#' calculations assume that the sampling frequency is constant.
+#' @param lambda a parameter for tuning the saccade
+#' detection.  It specifies which multiple of the standard deviation
+#' of the velocity distribution should be used as the detection
+#' threshold.
+#' @param smooth logical. If true the x- and y-coordinates will be
+#' smoothed using a moving average with window size 3 prior to saccade
+#' detection.
+#' @return data frame containing the detected fixations.  This data
+#' frame has the following columns:
+#'  \item{trial}{the trial to which the fixation belongs}
+#'  \item{start}{the time at which the fixation started}
+#'  \item{end}{the time at which the fixation ended}
+#'  \item{x}{the x-coordinate of the fixation}
+#'  \item{y}{the y-coordinate of the fixation}
+#'  \item{sd.x}{the standard deviation of the sample x-coordinates within the fixation}
+#'  \item{sd.y}{the standard deviation of the sample y-coordinates within the fixation}
+#'  \item{peak.vx}{the horizontal peak velocity that was reached within the fixation}
+#'  \item{peak.vy}{the vertical peak velocity that was reached within the fixation}
+#'  \item{dur}{the duration of the fixation}
+#' @keywords eye movements
+#' @export
+#' @examples
+#' data(eyemovements.raw)
+#' samples <- eyemovements.raw$samples
+#' head(samples)
+#' fixations <- detect.fixations(samples)
+#' head(fixations)
+#' first.trial <- samples$trial[1]
+#' first.trial.samples <- subset(samples, trial==first.trial)
+#' first.trial.fixations <- subset(fixations, trial==first.trial)
+#' with(first.trial.samples, plot(x, y, pch=20, cex=0.2, col="red"))
+#' with(first.trial.fixations, points(x, y, cex=1+sqrt(dur/10000)))
+detect.fixations <- function(samples, lambda=10, smooth=T) {
 
   # Discard unnecessary columns:
   samples <- samples[c("x", "y", "trial", "time")]
@@ -20,7 +64,7 @@ detect.fixations <- function(samples, vel.threshold=10, smooth=T) {
     samples$y[c(1,nrow(samples))] <- y
   }
     
-  samples <- detect.saccades(samples, vel.threshold)
+  samples <- detect.saccades(samples, lambda)
   
   if (all(!samples$saccade))
     stop("No saccades were detected.  Something went wrong.")
@@ -30,8 +74,6 @@ detect.fixations <- function(samples, vel.threshold=10, smooth=T) {
   fixations
   
 }
-
-# Package-internal functions:
 
 # This function takes a data frame of the samples and aggregates the
 # samples into fixations.  This requires that the samples have been
@@ -52,7 +94,7 @@ aggregate.fixations <- function(samples) {
   
   # Discard samples that occurred during saccades:
   samples <- samples[!samples$saccade,,drop=F]
-  
+
   fixations <- with(samples, data.frame(
     trial   = tapply(trial, fixation.id, function(x) x[1]),
     start   = tapply(time,  fixation.id, min),
@@ -61,8 +103,8 @@ aggregate.fixations <- function(samples) {
     y       = tapply(y,     fixation.id, mean),
     sd.x    = tapply(x,     fixation.id, sd),
     sd.y    = tapply(y,     fixation.id, sd),
-    peak.vx = tapply(vx,    fixation.id, max),
-    peak.vy = tapply(vy,    fixation.id, max),
+    peak.vx = tapply(vx,    fixation.id, function(x) x[which.max(abs(x))]),
+    peak.vy = tapply(vy,    fixation.id, function(x) x[which.max(abs(x))]),
     stringsAsFactors=F))
 
   fixations$dur <- fixations$end - fixations$start
@@ -80,8 +122,9 @@ aggregate.fixations <- function(samples) {
 #   whether the sample occurred during a saccade or not.
 # - Columns named vx and vy which indicate the horizontal and vertical
 #   speed.
-detect.saccades <- function(samples, vel.threshold) {
+detect.saccades <- function(samples, lambda) {
 
+  # Calculate horizontal and vertical velocities:
   vx <- filter(samples$x, -1:1/2)
   vy <- filter(samples$y, -1:1/2)
 
@@ -98,8 +141,8 @@ detect.saccades <- function(samples, vel.threshold) {
   msdx <- sqrt(median(vx**2, na.rm=T) - median(vx, na.rm=T)**2)
   msdy <- sqrt(median(vy**2, na.rm=T) - median(vy, na.rm=T)**2)
 
-  radiusx <- msdx * vel.threshold
-  radiusy <- msdy * vel.threshold
+  radiusx <- msdx * lambda
+  radiusy <- msdy * lambda
 
   sacc <- ((vx/radiusx)**2 + (vy/radiusy)**2) > 1
   samples$saccade <- ifelse(is.na(sacc), F, sacc)
